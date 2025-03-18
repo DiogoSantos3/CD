2c69193e5bbdc1040ffeb50bab6557b3a7512056
diogosantos <dpsantos005@gmail.com>
2025-03-18 10:38:28 +0000

    ex3b

---
+++ Aula_Pratica_1/ex3b.py
@@ -0,0 +1,12 @@
+def mdc(a,b):
+    #máximo divisor comum
+    if b == 0:
+        return a
+    
+    else:
+        return mdc(b, a%b)
+    
+
+if __name__ == "__main__":
+    #mdc(10,5)
+    print(mdc(48,18))
