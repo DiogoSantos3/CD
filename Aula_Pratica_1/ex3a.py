6e8f45e8fd292e9d88cab82d006c071043afd534
diogosantos <dpsantos005@gmail.com>
2025-03-18 10:31:38 +0000

    ex3a-aula-pratica-1

---
+++ Aula_Pratica_1/ex3a.py
@@ -0,0 +1,14 @@
+def num_func(left,right):
+    #multiplos de 4 no intervalo de left-right
+    for i in range(left,right+1):
+        if i%4==0:
+            print(i)
+
+    print("=====================================")
+
+
+if __name__ == "__main__":
+    num_func(1,10)
+    num_func(10,20)
+    num_func(20,30)
+    num_func(30,40)
