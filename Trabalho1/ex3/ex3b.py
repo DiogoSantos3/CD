d7d17094a30e490b2dac53927e80d6f7066523c8
diogosantos <dpsantos005@gmail.com>
2025-04-17 13:21:33 +0100

    ex3bi

---
+++ Trabalho1/ex3/ex3b.py
@@ -0,0 +1,22 @@
+#=============== i ===============
+import string
+
+def gerar_palavra_passe():
+    tamanho = random.randint(10, 14)
+    alfabeto = string.ascii_letters + string.digits + "!@#$%&*?"
+    return ''.join(random.choice(alfabeto) for _ in range(tamanho))
+
+# Gerar 10 exemplos
+for i in range(10):
+    print(f"PW{i+1}: {gerar_palavra_passe()}")
+
+
+#=============== ii ===============
+
+
+#=============== iii ===============
+
+
+#=============== iv ===============
+
+
